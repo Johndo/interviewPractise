007841f3114f32e6b51c4e810a2b4453f644880c
John Doherty <johndoherty@Johns-MacBook-Pro.local>
2020-09-20 18:43:27 -0400

    Initial Commit

---
+++ interviewPractise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interviewPractise
+//
+//  Created by John Doherty on 9/20/20.
+//  Copyright © 2020 John Doherty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
